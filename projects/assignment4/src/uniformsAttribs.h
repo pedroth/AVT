52fd73e8d02d771ed2b37008d63f5f60566fa1c3
pedroth <hiperpc@yahoo.com>
2013-11-30 14:45:41 +0000

    assignment4 addition

--- projects/assignment4/src/uniformsAttribs.h
+++ projects/assignment4/src/uniformsAttribs.h
@@ -10,29 +10,29 @@
 
 namespace VertexAttribs {
 	namespace Names {
-		extern const std::string POSITION;
+		extern const std::string POSITION = "inPos";
 		//extern const std::string COLOR;
-		extern const std::string NORMAL;
-		extern const std::string TEXCOORD;
+		extern const std::string NORMAL = "inNormal";
+		extern const std::string TEXCOORD = "inTex";
 	}
 	namespace Indexes {
-		extern const GLuint POSITION;
+		extern const GLuint POSITION = 1;
 		//extern const GLuint COLOR;
-		extern const GLuint NORMAL;
-		extern const GLuint TEXCOORD;
+		extern const GLuint NORMAL = 2;
+		extern const GLuint TEXCOORD = 3;
 	}
 }
 
 namespace Uniforms {
-	extern const std::string MATRIX;
-	extern const std::string COLOR;
+	extern const std::string MATRIX = "matrix";
+	extern const std::string COLOR = "color";
 }
 
 namespace UniformBlocks {
 	namespace Names {
-		extern const std::string SHARED_MATRICES;
+		extern const std::string SHARED_MATRICES = "sharedMatrix";
 	}
 	namespace BindPoints {
-		extern const GLuint SHARED_MATRICES;
+		extern const GLuint SHARED_MATRICES = 0;
 	}
 }
