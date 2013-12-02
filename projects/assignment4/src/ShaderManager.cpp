c996c6642955b02344338cc29242da104b085e25
pedroth <hiperpc@yahoo.com>
2013-12-02 11:47:31 +0000

    shaderManager

---
+++ projects/assignment4/src/ShaderManager.cpp
@@ -0,0 +1,19 @@
+#include "ShaderManager.h"
+
+ShaderManager* shaderManager = nullptr;
+ShaderManager::ShaderManager() : shaders() { }
+ShaderManager* ShaderManager::getInstance() {
+	if (shaderManager == nullptr)
+		shaderManager = new ShaderManager();
+	return shaderManager;
+}
+void ShaderManager::add(std::string name, ShaderProgram* shader) {
+	shaders[name] =  shader;
+}
+ShaderProgram*  ShaderManager::get(std::string name) {
+	shaderMapType::iterator it = shaders.find(name);
+	if (it == shaders.end())
+		return nullptr;
+	else
+		return it->second;
+}
---
+++ projects/assignment4/src/ShaderManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<map>
+#include"engine.h"
+
+
+class ShaderManager {
+private :
+	typedef std::map<std::string, ShaderProgram *> shaderMapType;
+	shaderMapType  shaders;
+	static ShaderManager* shaderManager;
+	ShaderManager();
+public:
+	ShaderManager* getInstance();
+	void add(std::string name, ShaderProgram* shader);
+	ShaderProgram*  get(std::string name);
+};
