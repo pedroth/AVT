ca4d97fc4e8edaafad59ff057fdbf7b936a235ee
pedroth <hiperpc@yahoo.com>
2013-12-02 09:36:20 +0000

    engine.h

--- projects/assignment4/src/engine.h
+++ projects/assignment4/src/engine.h
@@ -1,7 +1,9 @@
 #pragma once
 #include<glm.hpp>
 #include<gtc\quaternion.hpp>
+#include<gtc\matrix_transform.hpp>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include "RenderModel.h"
 #include "uniformsAttribs.h"
+#include "ShaderProgram.h"
